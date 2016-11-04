b9f6f6dc3be0c3f65d82ade84fcaf95de65e94b0
Drixide77 <adria.abella.93@gmail.com>
2016-11-04 18:57:07 +0100

    Small test

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 int main() {
+	string input;
+	bool running = true;
+
+  cout << "Welcome to BORK: A Tale of Tails!" << endl;
+	while (running) {
+    cin >> input;
+    if (input == "bazinga") {
+      cout << "How did you know?!" << endl;
+      running = false;
+    }
+    else cout << "I'm not listenning! Lalala~!" << endl;
+	}
+	
 	return 0;
 }
