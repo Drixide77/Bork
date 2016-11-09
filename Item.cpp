3e6aed5a94963fdd2fdf09e6966e525a59712a4c
Drixide77 <adria.abella.93@gmail.com>
2016-11-09 08:48:24 +0100

    Possible mistake solved.

--- Item.cpp
+++ Item.cpp
@@ -3,7 +3,7 @@
 Item::Item(const string& name, const string& description, Thing* container): Thing(name, description, container)
 {
   this->type = ITEM;
-  (container)->contains.push_back(this);
+  // (container)->contains.push_back(this); Thing already does this!!!
 }
 
 Item::~Item()
